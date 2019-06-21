583a2bec86ae2ed503f845eb61b9c4af0b9ff40c
Badri Vasistha <badri.vasistha@gmail.com>
2019-06-20 22:15:20 -0700

    dependent package files, gitignore files added.

---
+++ keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SPOTIFY_ID: process.env.SPOTIFY_ID,
+    SPOTIFY_SECRET: process.env.SPOTIFY_SECRET
+}
+
+// exports.spotify = {
+//     id: process.env.SPOTIFY_ID,
+//     secret: process.env.SPOTIFY_SECRET
+//   };
